4054281da3365b3fe7a6802c3e11ed7332817b30
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-06-04 19:46:23 +0200

    Add greet_rust_helpers function

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! Rust Helpers Library
+
+/// Sends greetings from the Rust Helpers library.
+pub fn greet_rust_helpers(name: &str) -> String{
+    format!("Hello, {}! Welcome to Rust Helpers.", name)
+}
